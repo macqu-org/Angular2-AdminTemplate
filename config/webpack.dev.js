2375bc4e70ab0aa939527f89fb3b155e5bf3c4ff
Kumar Nitesh <contact@kumar-nitesh.com>
2017-03-22 22:23:56 -0400

    Converting to Webpack based solution - Code still failing

---
+++ config/webpack.dev.js
@@ -0,0 +1,83 @@
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var commonConfig = require('./webpack.common.js');
+var helpers = require('./helper');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = webpackMerge(commonConfig, {
+  devtool: 'cheap-module-eval-source-map',
+    entry: {
+    'polyfills': './src/polyfills.ts',
+    'vendor': './src/vendor.ts',
+    'app': './src/main.ts'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: { configFileName: helpers.root('src', 'tsconfig.json') }
+          } , 'angular2-template-loader'
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=assets/[name].[hash].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: helpers.root('src', 'app'),
+        loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
+      },
+      {
+        test: /\.css$/,
+        include: helpers.root('src', 'app'),
+        loader: 'raw-loader'
+      }
+    ]
+  },
+
+  plugins: [
+    // Workaround for angular/angular#11580
+    new webpack.ContextReplacementPlugin(
+      // The (\\|\/) piece accounts for path separators in *nix and Windows
+      /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+      helpers.root('./src'), // location of your src
+      {} // a map of your routes
+    ),
+new ExtractTextPlugin('[name].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['app', 'vendor', 'polyfills']
+    }),
+
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: 'http://localhost:8080/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
+  },
+
+ 
+
+  devServer: {
+    historyApiFallback: true,
+    stats: 'minimal'
+  }
+});
--- src/app/app.component.spec.js
+++ src/app/app.component.spec.js
@@ -1,27 +1,13 @@
 "use strict";
-var app_component_1 = require('./app.component');
 var testing_1 = require('@angular/core/testing');
-var platform_browser_1 = require('@angular/platform-browser');
-describe('AppComponent', function () {
-    var de;
-    var comp;
-    var fixture;
-    beforeEach(testing_1.async(function () {
-        testing_1.TestBed.configureTestingModule({
-            declarations: [app_component_1.AppComponent]
-        })
-            .compileComponents();
-    }));
+var app_component_1 = require('./app.component');
+describe('App', function () {
     beforeEach(function () {
-        fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
-        comp = fixture.componentInstance;
-        de = fixture.debugElement.query(platform_browser_1.By.css('h1'));
+        testing_1.TestBed.configureTestingModule({ declarations: [app_component_1.AppComponent] });
     });
-    it('should create component', function () { return expect(comp).toBeDefined(); });
-    it('should have expected <h1> text', function () {
-        fixture.detectChanges();
-        var h1 = de.nativeElement;
-        expect(h1.innerText).toMatch(/angular/i, '<h1> should say something about "Angular"');
+    it('should work', function () {
+        var fixture = testing_1.TestBed.createComponent(app_component_1.AppComponent);
+        expect(fixture.componentInstance instanceof app_component_1.AppComponent).toBe(true, 'should create AppComponent');
     });
 });
 //# sourceMappingURL=app.component.spec.js.map
